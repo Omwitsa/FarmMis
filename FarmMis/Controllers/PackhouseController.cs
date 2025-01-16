b702163f2b05212b8c3d5869d235993b8b7e9697
Wilson <shadixkim@gmail.com>
2025-01-16 11:48:38 +0300

    Update packlist details

--- FarmMis/Controllers/PackhouseController.cs
+++ FarmMis/Controllers/PackhouseController.cs
@@ -50,26 +50,120 @@ namespace FarmMis.Controllers
 
         [Authorize(Roles = "Packlist")]
         [HttpPost]
-        public IActionResult Packlist([Bind("Date")] PacklistVm packlist)
+        public async Task<IActionResult> Packlist([Bind("Date")] PacklistVm packlist)
         {
-            
             var userId = HttpContext.User.FindFirst(StrValues.UserId)?.Value ?? "";
             menuBuilder.BuildMenus(this, userId, "Packlist");
-
-            var setup = _context.SysSetup.FirstOrDefault();
-            if (setup == null)
+            try
             {
-                _notyf.Error("Sorry, System settings not found");
+                var setup = _context.SysSetup.FirstOrDefault();
+                if (setup == null)
+                {
+                    _notyf.Error("Sorry, System settings not found");
+                    return View(packlist);
+                }
+                setup.Farm = setup?.Farm ?? "";
+                string date = packlist.Date.ToString("yyyyMMddHHmmss");
+                var packlistResults = _portalProxy.GetPackList(setup.Farm, date).Result;
+                var packlistobj = JsonConvert.DeserializeObject<dynamic>(packlistResults);
+
+                foreach (var client in packlistobj.clients)
+                {
+                    var code = $"{client.code}";
+                    if (!_context.Customers.Any(s => s.Code.ToUpper().Equals(code.ToUpper())))
+                    {
+                        await _context.Customers.AddAsync(new Customer
+                        {
+                            VegId = client.id,
+                            Name = client.name,
+                            Code = client.code,
+                        });
+                    }
+                }
+
+                foreach (var branch in packlistobj.branches)
+                {
+                    int id = branch.id;
+                    if (!_context.Branches.Any(s => s.VegId == id))
+                    {
+                        await _context.Branches.AddAsync(new Branch
+                        {
+                            VegId = id,
+                            Name = branch.name,
+                            CustomerId = branch.customerId,
+                        });
+                    }
+                }
+
+                foreach (var product in packlistobj.products)
+                {
+                    int id = product.id;
+                    if (!_context.Products.Any(s => s.VegId == id))
+                    {
+                        await _context.Products.AddAsync(new Product
+                        {
+                            VegId = id,
+                            Name = product.name,
+                            Code = product.code,
+                            Type = product.type,
+                            Category = product.category,
+                        });
+                    }
+                }
+
+                foreach (var dispatch in packlistobj.dispatch)
+                {
+                    int id = dispatch.dispatchheaderid;
+                    var savedPacklist = _context.Packlists.Include(p => p.PacklistLines).FirstOrDefault(p => p.VegHeaderId == id);
+                    if (savedPacklist == null)
+                    {
+                        var lines = new List<PacklistLine>();
+                        foreach (var line in dispatch.line)
+                        {
+                            lines.Add(new PacklistLine
+                            {
+                                VegLineId = line.lineid,
+                                VegHeaderId = line.headerid,
+                                ProductId = line.productid,
+                                BoxQty = line.boxqty,
+                                Barcode = line.barcode,
+                            });
+                        }
+
+                        await _context.Packlists.AddAsync(new Packlist
+                        {
+                            VegHeaderId = id,
+                            DispatchDate = dispatch.dispatchdate,
+                            CustomerId = dispatch.customerid,
+                            CustomerName = dispatch.customername,
+                            BranchId = dispatch.branchid,
+                            BranchName = dispatch.branchname,
+                            PacklistLines = lines,
+                        });
+                    }
+                    else
+                    {
+                        foreach (var line in dispatch.line)
+                        {
+                            int lineId = line.lineid;
+                            var packlistLine = savedPacklist.PacklistLines.FirstOrDefault(l => l.VegLineId == lineId);
+                            if (packlistLine != null)
+                            {
+                                packlistLine.Barcode = line.barcode;
+                            }
+                        }
+                    }
+                }
+
+                await _context.SaveChangesAsync();
+                _notyf.Success("Synched successfully");
                 return View(packlist);
             }
-            setup.Farm = setup?.Farm ?? "";
-            string date = packlist.Date.ToString("yyyyMMddHHmmss");
-            var packlistResults = _portalProxy.GetPackList(setup.Farm, date).Result;
-            var packlistobj = JsonConvert.DeserializeObject<dynamic>(packlistResults);
-
-            var val = packlistobj.clients;
-            //ViewBag.appliedLeaves = jdata.Data.applications;
-            return View();
+            catch (Exception)
+            {
+                _notyf.Error("Sorry, An error occurred");
+                return View(packlist);
+            }
         }
     }
 }
